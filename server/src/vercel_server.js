0343eb3ee822b99f9dd8f4b51cd23a23d2418027
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-09-02 09:44:45 +0300

    cors + helment verces setup
    cors + helment verces setup

--- server/src/vercel_server.js
+++ server/src/vercel_server.js
@@ -25,6 +25,38 @@ const typeDefs = fs.readFileSync(
 const app = express();
 const httpServer = http.createServer(app);
 
+// Использование Helmet для установки заголовков безопасности, включая CSP
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "https:"],
+      },
+    },
+  })
+);
+// Настройки CORS
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: [
+      "X-CSRF-Token",
+      "X-Requested-With",
+      "Accept",
+      "Accept-Version",
+      "Content-Length",
+      "Content-MD5",
+      "Content-Type",
+      "Date",
+      "X-Api-Version",
+      "locale",
+    ],
+  })
+);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -40,39 +72,6 @@ const server = new ApolloServer({
 async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
-
-  // Использование Helmet для установки заголовков безопасности, включая CSP
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: ["'self'"],
-          scriptSrc: ["'self'", "'unsafe-inline'"],
-          imgSrc: ["'self'", "data:", "https:"],
-        },
-      },
-    })
-  );
-
-  // Настройки CORS
-  app.use(
-    cors({
-      origin: "*",
-      credentials: true,
-      allowedHeaders: [
-        "X-CSRF-Token",
-        "X-Requested-With",
-        "Accept",
-        "Accept-Version",
-        "Content-Length",
-        "Content-MD5",
-        "Content-Type",
-        "Date",
-        "X-Api-Version",
-        "locale",
-      ],
-    })
-  );
 
   // Настройка статических файлов для клиентской части
   app.use(express.static(path.join(__dirname, "../../client/build")));
